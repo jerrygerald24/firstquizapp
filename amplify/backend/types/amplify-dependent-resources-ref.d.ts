9389a5c30b7e6ac23fddfbbf3d328a2df6fda274
jerry24 <jerry.infant.gerald@gmail.com>
2024-04-26 13:39:10 -0700

    Simple Quiz App with Cognito

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,16 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "myquizapp0952eaf8": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    },
+    "userPoolGroups": {
+      "tryingnewpoolGroupRole": "string"
+    }
+  }
+}
